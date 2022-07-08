5d1e4af6fbd6a8986ddc6ef21657eb3a85912d7a
Mitushi-23 <mitushi2agrawal@gmail.com>
2022-07-08 23:25:55 +0530

    added lazy loader and resolved order page issues

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,12 +3,13 @@ import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Rating from "../components/Rating";
 
+
 const Product = ({ product }) => {
   return (
     <>
       <Card className="my-3 p-2 rounded">
         <Link to={`products/${product._id}`}>
-          <Card.Img src={product.image} variant="top" />
+          <Card.Img src={product.image} variant="top" loading="lazy"/>
         </Link>
 
         <Card.Body>
