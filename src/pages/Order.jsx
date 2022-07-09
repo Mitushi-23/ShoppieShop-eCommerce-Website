6b7defe8a6886f5c2ecd34b1feeed18ca288357b
Mitushi-23 <mitushi2agrawal@gmail.com>
2022-07-09 11:47:41 +0530

    resolved the issue of paypal

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -42,7 +42,7 @@ const Order = () => {
   
   useEffect(() => {
     const addPaypalScript = async()=>{
-      const {data:clientId} = await axios.get('/api/config/paypal')
+      const {data:clientId} = await axios.get('/config/paypal')
       const script = document.createElement('script')
       script.type = 'text/javascript'
       script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
