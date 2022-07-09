50ab69b74f03066bdb34d7b796558f2155a164a9
Mitushi-23 <mitushi2agrawal@gmail.com>
2022-07-09 18:50:42 +0530

    added more products and added filter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar, Container, NavDropdown, Form } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../actions/userAction";
+import { useState } from "react";
+import {productCategory} from '../actions/productActions'
 
 const Header = () => {
+  const [category, setcategory] = useState("null")
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const logoutHandler = () => {
-    navigate("/login")
-   dispatch(logout());
+    navigate("/login");
+    dispatch(logout());
   };
+  const handleClick=(e)=>{
+    setcategory(e.target.value);
+    dispatch(productCategory(e.target.value));
+  }
   return (
     <>
       <Navbar bg="" expand="lg" collapseOnSelect>
@@ -20,10 +27,31 @@ const Header = () => {
           <Link to="/" style={{ textDecoration: "none" }}>
             <Navbar.Brand>ONLINE SHOP</Navbar.Brand>
           </Link>
+          <Form.Select
+            aria-label="Default select example"
+            style={{ width: "75%", margin: "auto" }}
+            onChange={(e)=>{handleClick(e)}}
+          >
+            <option value="null">Filter By Category</option>
+            <option value="men">Men's Fashion</option>
+            <option value="women">Women's Fashion</option>
+            <option value="electronics">Electronics</option>
+            <option value="Kitchen & Dining">Kitchen & Dining</option>
+            <option value="jewelery">Jewelery & Watches</option>
+            <option value="bags & shoes">Bags & Shoes</option>
+            <option value="home & furniture">Home & Furniture</option>
+            <option value="tools & hardware">Tools & Hardware</option>
+          </Form.Select>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <p to=""  style={{ textDecoration: "none" }} onClick={()=>userInfo? navigate("/cart"):navigate("/login")}>
+              <p
+                to=""
+                style={{ textDecoration: "none" }}
+                onClick={() =>
+                  userInfo ? navigate("/cart") : navigate("/login")
+                }
+              >
                 <Nav.Link href="#home" className="text-light">
                   <i className="fa-solid fa-cart-shopping text-dark"></i>
                   &nbsp;Cart
@@ -31,7 +59,11 @@ const Header = () => {
               </p>
               {userInfo ? (
                 <NavDropdown title={userInfo.name} id="username">
-                  <Link to="/profile" style={{ textDecoration: "none" }} onClick={()=>(navigate("/profile"))}>
+                  <Link
+                    to="/profile"
+                    style={{ textDecoration: "none" }}
+                    onClick={() => navigate("/profile")}
+                  >
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </Link>
                   <NavDropdown.Item onClick={logoutHandler}>
